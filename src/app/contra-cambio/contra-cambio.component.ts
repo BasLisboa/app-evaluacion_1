578a36a45ad298ad723d4483d5a0a45fbc824507
DanielGalvezzz <da.galvezg@duocuc.cl>
2024-08-30 15:07:38 -0400

    hola

---
+++ src/app/contra-cambio/contra-cambio.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contra-cambio',
+  templateUrl: './contra-cambio.component.html',
+  styleUrls: ['./contra-cambio.component.scss'],
+})
+export class ContraCambioComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
